ee042ca44332bb15de5286160b4df102fbb390a1
MichaelShawUK <MichaelShawUK@gmail.com>
2023-03-16 17:14:44 +0000

    Allow user to add message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date().toString().slice(0, 21),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().toString().slice(0, 21),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.author,
+    added: new Date().toString().slice(0, 21),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
