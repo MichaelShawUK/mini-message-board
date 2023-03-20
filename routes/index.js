759e584ef38f62f00e0c7082306d2cc50fb9b7a1
MichaelShawUK <MichaelShawUK@gmail.com>
2023-03-20 10:46:30 +0000

    Restore to working order

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-const fruit = process.env.fruit;
-
 const messages = [
   {
     text: "Hi there!",
@@ -18,7 +16,7 @@ const messages = [
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Mini Message Board", messages, fruit });
+  res.render("index", { title: "Mini Message Board", messages });
 });
 
 router.get("/new", function (req, res, next) {
